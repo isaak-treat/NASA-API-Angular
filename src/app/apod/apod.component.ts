264df7a56c4dff4b0f4ef793ee7fef2ac9814cd2
isaak-treat <isaaktreaty@gmail.com>
2020-08-05 14:46:13 -0600

    Styling to expanded weather

--- src/app/apod/apod.component.ts
+++ src/app/apod/apod.component.ts
@@ -7,7 +7,7 @@ import { ApiService } from '../api.service';
   styleUrls: ['./apod.component.css']
 })
 export class ApodComponent implements OnInit {
-  photoOfDay = {  };
+  photoOfDay = { };
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
